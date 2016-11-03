e81d3ab40aedde7b39b0ff903ec9d79a7825ba62
jtomkiew <spratfear@gmail.com>
2016-11-03 22:13:49 +0100

    Cokolwiek

---
+++ MyState/MyState.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStateImplementation
+{
+    public class MyState : IEquatable<MyState>
+    {
+        private EState state;
+
+        public static MyState False = new FalseState();
+        public static MyState True = new TrueState();
+        public static MyState Null = new NullState();
+        public static MyState Undefined = new UndefinedState();
+
+        //public MyState() {
+        //    Console.WriteLine("Constructed!");
+        //}
+
+        public bool Equals(MyState other)
+        {
+            if (other == null)
+                return false;
+
+            if (this.state == other.state)
+                return true;
+            else
+                return false;
+        }
+
+        public override bool Equals(Object obj)
+        {
+            if (obj == null)
+                return false;
+
+            MyState myStateObj = obj as MyState;
+            if (myStateObj == null)
+                return false;
+            else
+                return Equals(myStateObj);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.state.GetHashCode();
+        }
+
+        public static bool operator == (MyState state1, MyState state2)
+        {
+            if (((object)state1) == null || ((object)state2) == null)
+                return Object.Equals(state1, state2);
+
+            return state1.Equals(state2);
+        }
+
+        public static bool operator != (MyState state1, MyState state2)
+        {
+            if (((object)state1) == null || ((object)state2) == null)
+                return !Object.Equals(state1, state2);
+
+            return !(state1.Equals(state2));
+        }
+
+        protected enum EState
+        {
+            False,
+            True,
+            Null,
+            Undefined
+        }
+
+        class FalseState : MyState
+        {
+            public FalseState()
+            {
+                state = EState.False;
+            }
+        }
+
+        class TrueState : MyState
+        {
+            public TrueState()
+            {
+                state = EState.True;
+            }
+        }
+
+        class NullState : MyState
+        {
+            public NullState()
+            {
+                state = EState.Null;
+            }
+        }
+
+        class UndefinedState : MyState
+        {
+            public UndefinedState()
+            {
+                state = EState.Undefined;
+            }
+        }
+    }
+}
---
+++ MyState/MyStateTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Diagnostics;
+
+namespace MyStateImplementation
+{
+    class MyStateTests
+    {
+        [Test]
+        public void TwoDifferentStates()
+        {
+            var TrueState = MyState.True;
+            var FalseState = MyState.False;
+
+            Assert.AreNotEqual(TrueState, FalseState);
+        }
+
+        [Test]
+        public void TwoSameStates()
+        {
+            var TrueState = MyState.True;
+            var AnotherTrueState = MyState.True;
+
+            Assert.AreEqual(TrueState, AnotherTrueState);
+        }
+
+        [Test]
+        public void EqualityOperator()
+        {
+            var TrueState = MyState.True;
+            var AnotherTrueState = MyState.True;
+
+            Assert.True(TrueState == AnotherTrueState);
+        }
+
+        [Test]
+        public void InequalityOperator()
+        {
+            var TrueState = MyState.True;
+            var AnotherTrueState = MyState.True;
+
+            Assert.False(TrueState != AnotherTrueState);
+        }
+
+        [Test]
+        public void SameyOrNot()
+        {
+            MyState FirstState = MyState.Null;
+            MyState SecondState = MyState.True;
+
+            Assert.AreNotSame(FirstState, SecondState);
+
+            SecondState = MyState.Null;
+
+            Assert.AreSame(FirstState, SecondState);
+        }
+
+        [Test]
+        public void DoSomeLoops()
+        {
+            MyState FirstState;
+            var sw = new Stopwatch();
+            long counter = 0;
+            sw.Start();
+            while (sw.ElapsedMilliseconds < 50)
+            {
+                FirstState = MyState.True;
+                counter++;
+            }
+            sw.Stop();
+            Console.WriteLine($"Assigned {counter} times in a span of {sw.ElapsedMilliseconds / 1000.0} seconds.");
+        }
+    }
+}
